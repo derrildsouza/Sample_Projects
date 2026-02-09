e8e273b60d96ed5cb4455a45eaf15a1ba16342e3
derrildsouza <dsouzaderril@gmail.com>
2026-02-10 00:05:28 +0530

    adding cpp project for server client

--- server_client_cpp_DT/build/client/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derril/projects/Antigravity_tests/first/cpp_program/client
-# Build directory: /home/derril/projects/Antigravity_tests/first/cpp_program/build/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- server_client_cpp_DT/build/test/CMakeFiles/test_runner_cpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_runner_cpp.
-# This may be replaced when dependencies are built.
